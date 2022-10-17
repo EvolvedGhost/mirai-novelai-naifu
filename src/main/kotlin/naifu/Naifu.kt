8243c478496d8d532632694b503df804708fe004
EvolvedGhost <727511070@qq.com>
2022-10-17 23:29:09 +0800

    修复随机数BUG

--- src/main/kotlin/naifu/Naifu.kt
+++ src/main/kotlin/naifu/Naifu.kt
@@ -55,7 +55,7 @@ class Naifu() {
             val gson = GsonBuilder().create()
             val newSeed = if (seed == -1L) {
                 val random = SecureRandom()
-                random.nextLong() % 4294967296
+                random.nextInt().toLong() + 2147483648
             } else {
                 seed
             }
@@ -92,7 +92,7 @@ class Naifu() {
             val gson = GsonBuilder().create()
             val newSeed = if (seed == -1L) {
                 val random = SecureRandom()
-                random.nextLong() % 4294967296
+                random.nextInt().toLong() + 2147483648
             } else {
                 seed
             }
